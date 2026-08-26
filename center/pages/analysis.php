f4e467e3b45a8cb3179eb2d52710e93f7f202049
Viki <mgood251217@gmail.com>
2026-08-26 10:27:45 +0700

    fix : analysis move to orders

--- center/pages/analysis.php
+++ center/pages/analysis.php
@@ -124,7 +124,7 @@ $data = $controller->getIndexData($access, $startDateGet, $endDateGet);
 </div>
 
 <div class="filter-card">
-    <form method="get" action="/orders" class="filter-form">
+    <form method="get" action="/analysis" class="filter-form">
         <div class="form-group">
             <label for="start_date">Dari Tanggal</label>
             <input type="date" id="start_date" name="start_date" class="form-control" value="<?= htmlspecialchars($data['startDate']) ?>" onchange="this.form.submit()">
